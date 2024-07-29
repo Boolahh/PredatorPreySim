af089652354e96d2a8a643d5dfd8641e84857c19
Abdullah Abdullah <mangoabdullah@gmail.com>
2024-07-28 20:00:17 -0600

    File that defines the prey class

---
+++ prey.py
@@ -0,0 +1,13 @@
+import random
+from config import GRID_SIZE
+
+class Prey:
+    def __init__(self, x, y, energy):
+        self.x = x
+        self.y = y
+        self.energy = energy
+
+    def move(self, energy_consumption):
+        self.x = (self.x + random.choice([-1, 0, 1])) % GRID_SIZE
+        self.y = (self.y + random.choice([-1, 0, 1])) % GRID_SIZE
+        self.energy -= energy_consumption
