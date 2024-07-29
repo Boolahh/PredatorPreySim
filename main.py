57918339044b5d61063453b893ea1bb11cc7eea8
Abdullah Abdullah <mangoabdullah@gmail.com>
2024-07-28 19:59:05 -0600

    Split the base program into multiple classes to enhance modularity

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import Scale, Label, Button
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.animation as animation
+from simulation import Simulation
+
+# Main execution
+root = tk.Tk()
+root.title("Predator-Prey Simulation Controls")
+simulation = Simulation(root)
+root.mainloop()
